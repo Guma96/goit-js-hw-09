e3dab8809d50b6a52cccc410e655677c011d0513
Guma96 <mateusz.kutarba96@gmail.com>
2023-03-13 16:42:25 +0100

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,78 +1,67 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const date = document.querySelector('#datetime-picker');
-const btn = document.querySelector('[data-start]');
-const day = document.querySelector('[data-days]');
-const hour = document.querySelector('[data-hours]');
-const min = document.querySelector('[data-minutes]');
-const sec = document.querySelector('[data-seconds]');
-const spans = document.querySelectorAll('.value');
+const datetimePicker = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('[data-start]');
 
-let timerId = null;
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMinutes = document.querySelector('[data-minutes]');
+const timerSeconds = document.querySelector('[data-seconds]');
 
-btn.disabled = true;
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
 
-flatpickr(date, {
+const timer = {
+  timestamp: null,
+  interval: null,
+
+  ready(stamp) {
+    this.timestamp = stamp;
+    startButton.addEventListener('click', timer.start);
+  },
+
+  start() {
+    startButton.disabled = true;
+    timer.interval = setInterval(() => {
+      let diff = timer.timestamp - Date.now();
+
+      if (diff < 1000) {
+        timer.stop();
+        Notify.failure('Time is up');
+      }
+
+      timerDays.textContent = pad(Math.floor(diff / 86400000));
+      timerHours.textContent = pad(Math.floor((diff % 86400000) / 3600000));
+      timerMinutes.textContent = pad(
+        Math.floor(((diff % 86400000) % 3600000) / 60000)
+      );
+      timerSeconds.textContent = pad(Math.floor((diff % 60000) / 1000));
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(timer.interval);
+    startButton.removeEventListener('click', timer.start);
+  },
+};
+
+flatpickr(datetimePicker, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] <= Date.now()) {
-      Notiflix.Notify.failure('Please choose a date in the future');
-      btn.disabled = true;
-    } else {
-      btn.disabled = false;
+    if (selectedDates[0] > Date.now()) {
+      startButton.disabled = false;
 
-      Notiflix.Notify.success('Lets go?');
+      timer.stop();
+      timer.ready(selectedDates[0]);
+      return;
     }
+
+    startButton.disabled = true;
+    Notify.failure('Please choose a date in the future');
   },
 });
-
-btn.addEventListener('click', onBtnStartClick);
-
-function onBtnStartClick() {
-  spans.forEach(item => item.classList.toggle('end'));
-  btn.disabled = true;
-  date.disabled = true;
-  timerId = setInterval(() => {
-    const choosenDate = new Date(date.value);
-    const timeToFinish = choosenDate - Date.now();
-    const { days, hours, minutes, seconds } = convertMs(timeToFinish);
-
-    day.textContent = addLeadingZero(days);
-    hour.textContent = addLeadingZero(hours);
-    min.textContent = addLeadingZero(minutes);
-    sec.textContent = addLeadingZero(seconds);
-
-    if (timeToFinish < 1000) {
-      spans.forEach(item => item.classList.toggle('end'));
-      clearInterval(timerId);
-      date.disabled = false;
-    }
-  }, 1000);
-}
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-function addLeadingZero(value) {
-  return `${value}`.padStart(2, '0');
-}
